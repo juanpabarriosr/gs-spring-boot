31e26aa60683deef1d61f285dffeb5c769074032
juanpabarriosr <juanpabarriosr@gmail.com>
2020-05-03 22:39:53 -0500

    Se agregó Jp1

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Spring Boot Jp!";
+        return "Hello from Spring Boot Jp1!";
     }   
 }
